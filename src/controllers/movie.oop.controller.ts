bcec2275dca472cf41d37a169230627ae4bf3137
egonella <emiliano.gonella@gmail.com>
2025-03-24 17:03:46 -0300

    added new movie services. Migrated remaining crud functions and created routes

--- src/controllers/movie.oop.controller.ts
+++ src/controllers/movie.oop.controller.ts
@@ -4,6 +4,9 @@ import { ApiError } from "../middleware/error.middleware";
 import { Request, Response, NextFunction } from "express";
 import { findFile, resolvePath, scanDirectoryForMovies } from "../utils/scan-functions";
 import fs from 'fs';
+import path from 'path';
+import os from 'os';
+import { getDirectoryStats } from "../utils/get-directory-stats";
 
 export class MovieController {
   private movieService: MovieService;
@@ -34,16 +37,80 @@ export class MovieController {
     }
   }
 
+  getAllMovies = async(request: Request, response: Response, next: NextFunction) => {
+    try {
+      const movies = await this.movieService.getAllMovies();
+
+      if (!movies) {
+        return next(new ApiError(404, 'No se encontraron las películas'));
+      }
+
+      return response.status(200).json(movies);
+    } catch (error) {
+      console.error('Error al obtener las películas:', error);
+      return next(new ApiError(500, 'Error al obtener las películas'));
+    }
+  }
+
+  getMoviesByUserId = async(request: Request, response: Response, next: NextFunction) => {
+    try {
+      const { userId } = request.body;
+
+      const movies = await this.movieService.getMoviesByUserId(userId);
+
+      if (!movies) {
+        return next(new ApiError(404, 'No se encontraron las películas del usuario'));
+      }
+
+      return response.status(200).json(movies);
+    } catch (error) {
+      console.error('Error al obtener las películas del usuario:', error);
+      return next(new ApiError(500, 'Error al obtener las películas del usuario'));
+    }
+  }
+
+  updateMovie = async(request: Request, response: Response, next: NextFunction) => {
+    try {
+      const { title, description, filePath, user, movieId } = request.body;
+      const userId = user.userId;
+
+      if (!movieId) {
+        return next(new ApiError(400, 'ID de la película no proporcionado'));
+      }
+
+      const movie = await this.movieService.updateMovie({
+        title,
+        description,
+        filePath,  
+        userId,
+      });
+
+      return response.status(200).json(movie);
+    } catch (error) {
+      console.error('Error al actualizar la película:', error);
+      return next(new ApiError(500, 'Error al actualizar la película'));
+    }
+  }
+
+  deleteMovie = async(request: Request, response: Response, next: NextFunction) => {
+    try {
+      const { movieId } = request.body;
+      const movie = await this.movieService.deleteMovie(movieId);
+      if (!movie) {
+        return next(new ApiError(404, 'La película no fue encontrada'));
+      }
+      return response.status(200).json(movie);
+    } catch (error) {
+      console.error('Error al eliminar la película:', error);
+      return next(new ApiError(500, 'Error al eliminar la película'));
+    }
+  }
+
   scanDirectoryCtrl = async (request: Request, response: Response, next: NextFunction) => {
     try {
       const { directoryPath, user } = request.body;
-
-      console.log('directoryPath', directoryPath);
-  
       const userId = user.userId;
 
-      console.log('userId', userId);
-  
       if (!userId) {
         return next(new ApiError(401, 'Usuario no autenticado'));
       }
@@ -53,8 +120,6 @@ export class MovieController {
       }
   
       const finalDirectoryPath = resolvePath(directoryPath);
-
-      console.log('finalDirectoryPath', finalDirectoryPath);
   
       if (!findFile(finalDirectoryPath)) {
         return next(new ApiError(404, 'Directorio no encontrado'));
@@ -79,6 +144,71 @@ export class MovieController {
       return response.status(200).json(result);
   
     } catch (error) {
+      return next(new ApiError(500, 'Error al escanear el directorio'));
+    }
+  }
+
+  listHomeFoldersCtrl = async (request: Request, response: Response, next: NextFunction) => {
+    try {  
+      const homeDir = os.homedir();
+      const foldersToFind = ["Downloads", "Videos", "Download", "Video", "Descargas", "Vídeos"];
+  
+      if (os.platform() === "win32") {
+        foldersToFind.push("Mis descargas", "Mis vídeos", "Mis videos");
+      }
+  
+      const foundFolders = [];
+  
+      for (const folder of foldersToFind) {
+        const folderPath = path.join(homeDir, folder);
+  
+        if (fs.existsSync(folderPath) && fs.statSync(folderPath).isDirectory()) {
+  
+          const stats = getDirectoryStats(folderPath);
+          foundFolders.push({
+            name: folder,
+            path: folderPath,
+            stats
+          })
+        }
+      }
+  
+      if (os.platform() === "win32") {
+        const driveLetters = "CDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (let i = 0; i < driveLetters.length; i++) {
+          const driveLetter = driveLetters[i];
+          const drivePath = `${driveLetter}:\\`;
+  
+          if (fs.existsSync(drivePath)) {
+            const commonFolders = ["Movies", "Películas", "Peliculas", "Videos", "Vídeos", "Download", "Downloads", "Video", "Descargas", "Media"];
+  
+            for (const folder of commonFolders) {
+              const folderPath = path.join(drivePath, folder);
+              if (fs.existsSync(folderPath) && fs.statSync(folderPath).isDirectory()) {
+                const stats = getDirectoryStats(folderPath);
+  
+                foundFolders.push({
+                  name: `${driveLetter}:\\${folder}`,
+                  path: folderPath,
+                  stats
+                })
+              }
+            }
+          }
+        }
+      }
+  
+      return response.status(200).json({
+        message: "Carpetas encontradas en el directorio principal",
+        data: {
+          homeDir,
+          platform: os.platform(),
+          folders: foundFolders,
+        }
+      })
+  
+    } catch (error) {
+      console.error("listHomeFoldersCtrl", error);
       return next(new ApiError(500, 'Error al escanear el directorio'));
     }
   }
--- src/services/MovieService.ts
+++ src/services/MovieService.ts
@@ -32,6 +32,77 @@ export class MovieService {
   }
 
   /**
+   * Metodo para obtener todas las peliculas
+   * @returns Array de peliculas
+   */
+
+  async getAllMovies(): Promise<Movie[]> {
+    const movies = await this.prisma.movie.findMany();
+
+    return movies.map((movie) => Movie.fromJSON(movie));
+  }
+
+  /**
+   * Metodo para obtener todas las peliculas de un usuario
+   * @param userId ID del usuario
+   * @returns Array de peliculas
+   */
+
+  async getMoviesByUserId(userId: number): Promise<Movie[]> {
+    const movies = await this.prisma.movie.findMany({
+      where: {
+        userId,
+      },
+    });
+
+    return movies.map((movie) => Movie.fromJSON(movie));
+  }
+
+  /**
+   * Método para actualizar una película
+   * @param id ID de la película
+   * @param data Datos de la película
+   * @returns Instancia de Movie
+   */
+
+  async updateMovie(data: {
+    title: string;
+    description?: string | null;
+    filePath: string;
+    userId: number;
+  }): Promise<Movie> {
+    const movie = await this.prisma.movie.update({
+      where: {
+        id: data.userId,
+      },
+      data: {
+        title: data.title,
+        description: data.description,
+        filePath: data.filePath,
+        userId: data.userId,
+      },
+    });
+
+    return Movie.fromJSON(movie);
+  }
+
+  /**
+   * Metodo para eliminar una pelicula
+   * @param id ID de la pelicula
+   * @returns Instancia de Movie
+   */
+
+  async deleteMovie(id: number): Promise<Movie> {
+    const movie = await this.prisma.movie.delete({
+      where: {
+        id,
+      },
+    });
+
+    return Movie.fromJSON(movie);
+  }
+
+  /**
    * Método para obtener una película por su ID
    * @param id ID de la película
    * @returns Instancia de Movie
